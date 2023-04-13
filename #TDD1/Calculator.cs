d86e182fe1ccb305d7b42fa8a3b82c27b0052841
pmia <contact@plantys.net>
2023-04-13 19:22:30 +1000

    +

--- #TDD1/Calculator.cs
+++ #TDD1/Calculator.cs
@@ -9,6 +9,9 @@ namespace _TDD1 {
   public int AddNumbers(int a, int b) {
    return a + b;
   }
+  public double AddNumbersDouble(double a, double b) {
+   return a + b;
+  }
   public bool IsOddNumber(int a) {
    return a %2 !=0;
   }
--- #TDD1_Test/Tests.cs
+++ #TDD1_Test/Tests.cs
@@ -12,6 +12,34 @@ namespace _TDD1_Test {
   [TestFixture]
   public class TDD1Test {
    [Test]
+   [TestCase(11.1,11.2 )]
+   public void AddNumbersDouble_InputTwoDouble_GetCorrectAddition(double a, double b) {
+    //Arrange
+    Calculator calc = new();
+
+    //Act
+double result = calc.AddNumbersDouble(a,b);
+
+    //Assert
+    Assert.AreEqual(22.3, result, 1);
+   }
+
+
+   [Test]
+   [TestCase(10, ExpectedResult = false)]
+   [TestCase(11, ExpectedResult = true)]
+   public bool IsOdd_InputAny_ReturnTrueIfOdd(int a) {
+    //Arrange
+    Calculator calc = new();
+
+    //Act
+    return calc.IsOddNumber(a);
+
+    //Assert
+
+   }
+   
+   [Test]
    public void AddNumbers_InputTwoInt_GetCorrectAddition() {
     //Arrange
     Calculator calc = new();
@@ -20,18 +48,20 @@ namespace _TDD1_Test {
     int result = calc.AddNumbers(10, 20);
 
     //Assert
-    Assert.AreEqual(30, result);
+    Assert.That(30, Is.EqualTo(result));
    }
    [Test]
-   public void IsOdd_InputOdd_GetCorrectAnswer() {
+   [TestCase(11)]
+   [TestCase(13)]
+   public void IsOdd_InputOdd_GetCorrectAnswer(int a) {
      //Arrange
      Calculator calc = new();
 
      //Act
-     bool result = calc.IsOddNumber(11);
+     bool result = calc.IsOddNumber(a);
 
      //Assert
-     Assert.AreEqual(true, result);
+     Assert.That( result, Is.True);
     }
    [Test]
    public void IsOdd_InputEven_GetCorrectAnswer() {
@@ -40,7 +70,7 @@ namespace _TDD1_Test {
     //Act
     bool result = calc.IsOddNumber(10);
     //Assert
-    Assert.AreEqual(false, result);
+    Assert.That(result, Is.False);
    }
   }
  }
